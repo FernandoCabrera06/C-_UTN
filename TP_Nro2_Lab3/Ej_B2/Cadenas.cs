9ab49cd5e40181ad1a2d456b6b9c45bd92ecbf1d
FernandoCabrera06 <80828369+FernandoCabrera06@users.noreply.github.com>
2021-08-29 03:11:26 -0300

    TP Nro 2 ejercicio B2 terminado

---
+++ TP_Nro2_Lab3/Ej_B2/Cadenas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Nro2_Lab3.Ej_B2
+{
+    class Cadenas
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese una cadena: ");
+            string cadena1 = Console.ReadLine();
+            Console.Write("Ingrese una cadena a buscar: ");
+            string cadena2 = validarExtensionCadena(cadena1, Console.ReadLine());
+
+            if (cadena1.Contains(cadena2))
+            {
+                Console.WriteLine("ENCONTRADO");
+            }
+            else
+            {
+                Console.WriteLine("NO ENCONTRADO");
+            }
+
+        }
+        private static string validarExtensionCadena(string cadena1, string cadena2)
+        {
+            while (!(cadena2.Length <= cadena1.Length))
+            {
+                Console.Write($"Ingrese una cadena menor o igual a {cadena1.Length} caracteres: ");
+                cadena2 = Console.ReadLine();
+            }
+            return cadena2;
+        }
+    }
+}
