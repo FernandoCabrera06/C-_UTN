709123428b42cd85ba5a45b4cccab59fe90eea87
FernandoCabrera06 <80828369+FernandoCabrera06@users.noreply.github.com>
2021-08-29 19:08:46 -0300

    TP Nro 2 ejercicio C2 terminado

---
+++ TP_Nro2_Lab3/Ej_C2/Arreglos2APP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Nro2_Lab3.Ej_C2
+{
+    class Arreglos2APP
+    {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Ingrese cadenad de numeros separados por coma: ");
+            string listaNumeros = Console.ReadLine();
+
+            string[]  vectorNumeros = listaNumeros.Split(',');
+            int sumaValores = 0;
+            for (int i = 0; i < vectorNumeros.Length; i++)
+            {
+               sumaValores += int.Parse(vectorNumeros[i]);
+            }
+
+            Console.WriteLine($"Suma total: {sumaValores}");
+        }
+    }
+}
